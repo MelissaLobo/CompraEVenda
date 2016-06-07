33bedc1cb5c553da7a4fca5eeb5151b561b713f6
Melissa Lobo <mellobomel@gmail.com>
2016-06-07 02:09:41 -0300

    adicionando logica da Loja

---
+++ src/main/java/dao/ShopDao.java
@@ -0,0 +1,18 @@
+package dao;
+
+/**
+ * @author MelissaLobo mellobomel@gmail.com
+ */
+import java.io.Serializable;
+
+import org.springframework.stereotype.Repository;
+
+import model.Shop;
+
+/**
+ * @author MelissaLobo mellobomel@gmail.com
+ */
+@Repository
+public class ShopDao extends DaoGeneric<Shop, Serializable> {
+
+}
