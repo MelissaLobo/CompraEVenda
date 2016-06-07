35f1038cadff3e344ad77374c42dee88ee822217
Melissa Lobo <mellobomel@gmail.com>
2016-06-07 02:10:48 -0300

    adicionando controller da Loja

--- src/main/java/controller/ShopController.java
+++ src/main/java/controller/ShopController.java
@@ -1,15 +1,20 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import model.Shop;
+import service.ShopService;
 
 @Controller
 public class ShopController {
 
+	@Autowired
+	private ShopService shopService;
+	
 	@RequestMapping("/newShop")
 	public String showNewShop(Model model) {
 		model.addAttribute("shop", new Shop());
@@ -18,8 +23,8 @@ public class ShopController {
 	
 	@RequestMapping(value = "/createShop", method = RequestMethod.POST)
 	public String showCreateShop(Shop shop) {
-		
+		shopService.createAdress(shop);
 		return "profileUser";
-
 	}
+	
 }
