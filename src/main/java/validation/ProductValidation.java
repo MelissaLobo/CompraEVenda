03e155c4bb2e2b121f9ce03063669ce0ede9338f
Melissa Lobo <mellobomel@gmail.com>
2016-06-08 21:58:13 -0300

    adicionando validacao do Produto

---
+++ src/main/java/validation/ProductValidation.java
@@ -0,0 +1,22 @@
+package validation;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+
+import model.Product;
+
+public class ProductValidation {
+
+
+	public boolean supports(Class<?> clazz) {
+		return Product.class.isAssignableFrom(clazz);
+	}
+
+	public void validate(Object target, Errors errors) {
+		ValidationUtils.rejectIfEmpty(errors, "productName", "field.required");
+		ValidationUtils.rejectIfEmpty(errors, "price", "field.required");
+		ValidationUtils.rejectIfEmpty(errors, "description", "field.required");
+		ValidationUtils.rejectIfEmpty(errors, "category", "field.required");
+		Product product = (Product) target;
+	}
+}
