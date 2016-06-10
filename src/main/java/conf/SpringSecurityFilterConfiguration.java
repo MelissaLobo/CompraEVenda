33c139a818bba7bc74a8905b42b941c8c889422e
Melissa Lobo <mellobomel@gmail.com>
2016-06-10 10:13:17 -0300

    Adicionando Classe de Validacao do Spring Security

---
+++ src/main/java/conf/SpringSecurityFilterConfiguration.java
@@ -0,0 +1,7 @@
+package conf;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityFilterConfiguration extends AbstractSecurityWebApplicationInitializer{
+
+}
