ee607a8427eb91544dd6c5f6d181137ad4df28a9
Melissa Lobo <mellobomel@gmail.com>
2016-06-15 12:56:41 -0300

    adicionando Classe Cart no ComponentSacan

--- src/main/java/conf/AppWebConfiguration.java
+++ src/main/java/conf/AppWebConfiguration.java
@@ -1,5 +1,9 @@
 package conf;
 
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.guava.GuavaCacheManager;
 /**
  * @author MelissaLobo mellobomel@gmail.com
  */
@@ -14,25 +18,31 @@ import org.springframework.format.datetime.DateFormatter;
 import org.springframework.format.datetime.DateFormatterRegistrar;
 import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.format.support.FormattingConversionService;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import com.google.common.cache.CacheBuilder;
 
 import controller.HomeController;
 import dao.AdressDao;
 import dao.ProductDao;
 import dao.ShopDao;
 import dao.UserDao;
+import model.Cart;
 import service.ProductService;
 import service.UserService;
 
 // Classe de configuração gerenciada pelo spring
 
 @EnableWebMvc
-@ComponentScan(basePackageClasses = { HomeController.class, UserDao.class, UserService.class, ProductDao.class, ProductService.class, AdressDao.class, ShopDao.class })
+@ComponentScan(basePackageClasses = { HomeController.class, UserDao.class, UserService.class, ProductDao.class, ProductService.class, AdressDao.class, ShopDao.class, Cart.class })
 public class AppWebConfiguration extends WebMvcConfigurerAdapter {
 
 	@Bean
@@ -76,5 +86,26 @@ public class AppWebConfiguration extends WebMvcConfigurerAdapter {
 	@Bean
 	public MultipartResolver multipartResolver() {
 		return new StandardServletMultipartResolver();
+	}
+	
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+	
+	@Bean
+	public CacheManager cacheManager() {
+		CacheBuilder<Object, Object> builder = CacheBuilder.newBuilder()
+			.maximumSize(100)
+			.expireAfterAccess(5, TimeUnit.MINUTES);
+		GuavaCacheManager manager = new GuavaCacheManager();
+		manager.setCacheBuilder(builder);
+		
+		return manager;
+	}
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+		return new CookieLocaleResolver();
 	}
 }
