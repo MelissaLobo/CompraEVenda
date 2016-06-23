6c12ef356a5e9d4e1f232d8500ac285673ab8951
Melissa Lobo <mellobomel@gmail.com>
2016-06-23 20:12:14 -0300

    configurando spring security

--- src/main/java/conf/SecurityConfig.java
+++ src/main/java/conf/SecurityConfig.java
@@ -19,42 +19,38 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import dao.UserDao;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter implements UserDetailsService {
 
+	@Autowired
+	private UserDao userDao;
+	
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(this).passwordEncoder( new BCryptPasswordEncoder());
+		auth.userDetailsService(userDao).passwordEncoder( new BCryptPasswordEncoder());
 	}
-		
-
-
-	@Autowired
-	public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("melissa").password("mel123").roles("USER");
-		auth.inMemoryAuthentication().withUser("admin").password("root123").roles("ADMIN");
-		auth.inMemoryAuthentication().withUser("dba").password("root123").roles("ADMIN", "DBA");
-	}
-
-	/*@Override
-	public void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().antMatchers("/home").access("hasRole('ROLE_USER')").and().formLogin()
-				.loginPage("/login").permitAll().loginProcessingUrl("/dologin").permitAll()
-				.usernameParameter("username").passwordParameter("password").and().logout().logoutUrl("/logout")
-				.logoutSuccessUrl("/index").permitAll();
-	}*/
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-		.antMatchers("/profileUser").access("hasRole('ROLE_USER')")
-		.antMatchers("/cart").access("hasRole('ROLE_USER')")
+		http.csrf().disable().authorizeRequests()
+		.antMatchers("/profileUser").hasRole("ADMIN")
+		.antMatchers("/cart").hasRole("ADMIN")
+		.antMatchers("/accountCreated").hasRole("ADMIN")
+		.antMatchers("/addComment").hasRole("ADMIN")
+		.antMatchers("/adressCreated").hasRole("ADMIN")
+		.antMatchers("/newAdress").hasRole("ADMIN")
+		.antMatchers("/newShop").hasRole("ADMIN")
+		.antMatchers("/productCreated").hasRole("ADMIN")
+		.antMatchers("/profileUser").hasRole("ADMIN")
 		.antMatchers("/catalog").permitAll()
 		.antMatchers("/newAccount").permitAll()
 		.antMatchers("/index").permitAll()
 		.antMatchers("/js/**").permitAll()
 		.antMatchers("/css/**").permitAll()
+		.antMatchers("/template").permitAll()
 		.antMatchers("/fonts/**").permitAll()
 		.antMatchers("/").permitAll()
 		.anyRequest().authenticated()
