f8de62bac2be5a287fe4d925ee2d3b82ccfe3f20
Melissa Lobo <mellobomel@gmail.com>
2016-06-24 23:06:50 -0300

    refatorando

--- src/main/java/controller/ProductController.java
+++ src/main/java/controller/ProductController.java
@@ -6,9 +6,12 @@ package controller;
 import java.math.BigDecimal;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -30,18 +33,30 @@ public class ProductController {
 	}
 
 	@RequestMapping("/createProduct")
-	public String showCreateProduct(Product product) {
+	public String showCreateProduct(@Valid Product product, BindingResult result) {
+		if(result.hasErrors()){
+			return showNewProduct(null);
+		}
 		BigDecimal big = new BigDecimal(product.getPrice().toString());
 		productService.createProduct(product);
 		return "productCreated";
 	}
 
 	@RequestMapping(value = "/listProduct", method = RequestMethod.GET)
-	public ModelAndView ShowListProduct(Model model) {
+	public ModelAndView showListProduct(Model model) {
 		ModelAndView modelAndView = new ModelAndView("listProduct");
-		List<Product> lista = productService.listProduct();
-		modelAndView.addObject("listProduct", lista);
+		List<Product> list = productService.listProduct();
+		modelAndView.addObject("listProduct", list);
 
+		return modelAndView;
+	}
+	
+	@RequestMapping("/productDescription/{id}")
+	public ModelAndView description(@PathVariable("id") Long id) {
+		ModelAndView modelAndView = new ModelAndView("productDescription");
+		Product product = productService.getById(id);
+		modelAndView.addObject("product", product);
+	
 		return modelAndView;
 	}
 }
