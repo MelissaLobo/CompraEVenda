56263121600a02024863e7be6a9d2a95001a3e42
Melissa Lobo <mellobomel@gmail.com>
2016-06-26 06:12:00 -0300

    criando metodo findShopByUser

--- src/main/java/dao/ShopDao.java
+++ src/main/java/dao/ShopDao.java
@@ -5,14 +5,26 @@ package dao;
  */
 import java.io.Serializable;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.stereotype.Repository;
 
 import model.Shop;
+import model.User;
 
 /**
  * @author MelissaLobo mellobomel@gmail.com
  */
 @Repository
 public class ShopDao extends DaoGeneric<Shop, Serializable> {
+	
+	@PersistenceContext
+	private EntityManager manager;
+
+	public Shop findShopByUser(User user) {
+		return manager.createQuery("from Shop s where s.user.id = :userId", Shop.class)
+				.setParameter("userId", user.getId()).getSingleResult();
+	}
 
 }
