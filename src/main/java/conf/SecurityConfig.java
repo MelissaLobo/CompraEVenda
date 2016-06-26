c128829675a794dd97e4785626fe8e0b02e7c7ac
Melissa Lobo <mellobomel@gmail.com>
2016-06-26 06:18:30 -0300

    configurando enum

--- src/main/java/conf/SecurityConfig.java
+++ src/main/java/conf/SecurityConfig.java
@@ -1,36 +1,26 @@
 package conf;
 
-/**
- * @author MelissaLobo mellobomel@gmail.com
- */
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import dao.UserDao;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter implements UserDetailsService {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private UserDao userDao;
 	
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDao).passwordEncoder( new BCryptPasswordEncoder());
+		auth.userDetailsService(userDao).passwordEncoder( new Md5PasswordEncoder());
 	}
 
 	@Override
@@ -50,18 +40,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements User
 		.antMatchers("/index").permitAll()
 		.antMatchers("/js/**").permitAll()
 		.antMatchers("/css/**").permitAll()
-		.antMatchers("/template").permitAll()
 		.antMatchers("/fonts/**").permitAll()
 		.antMatchers("/").permitAll()
 		.anyRequest().authenticated()
 		.and().formLogin().loginPage("/login").permitAll()
+	    .usernameParameter("username")
+	    .passwordParameter("password")
+		.and().formLogin().defaultSuccessUrl("/catalog")
 		.and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
-	}
-	
-	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		return new User(username, "0cc175b9c0f1b6a831c399e269772661", true, true, true, true,
-				Arrays.asList(new SimpleGrantedAuthority("ROLE_USER")));
 	}
 
 }
