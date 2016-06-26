668fbc6dcc8e41f72a6de216b08a2f3404664d7b
Melissa Lobo <mellobomel@gmail.com>
2016-06-26 06:28:59 -0300

    metodo add product e shop

--- src/main/java/service/ShopService.java
+++ src/main/java/service/ShopService.java
@@ -8,16 +8,31 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import dao.ShopDao;
+import dao.UserDao;
+import model.Product;
 import model.Shop;
+import model.User;
 
 @Service("shopService")
 public class ShopService {
 
 	@Autowired
 	private ShopDao shopDao;
-
+	
+	@Autowired
+	private UserDao userDao;
+	
 	@Transactional
-	public void createAdress(Shop shop) {
+	public void createShop(Shop shop,long idUser) {
+		shop.setUser(userDao.find(idUser));
 		shopDao.create(shop);
 	}
+
+	@Transactional
+    public void addProduct(User user, Product product){
+    	Shop shopgerenciado = shopDao.findShopByUser(user);
+    	shopgerenciado.getProduct().add(product);
+    	shopDao.create(shopgerenciado);
+    }
+ 
 }
