d327ce05e6b809b1d86f78ba1dc4c58f279ee502
Melissa Lobo <mellobomel@gmail.com>
2016-06-26 06:33:35 -0300

    formatacao

--- src/main/java/controller/ShopController.java
+++ src/main/java/controller/ShopController.java
@@ -1,12 +1,14 @@
 package controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import model.Shop;
+import model.User;
 import service.ShopService;
 
 @Controller
@@ -14,17 +16,15 @@ public class ShopController {
 
 	@Autowired
 	private ShopService shopService;
-	
+
 	@RequestMapping("/newShop")
-	public String showNewShop(Model model) {
-		model.addAttribute("shop", new Shop());
-		return "newShop";
+	public ModelAndView showNewShop() {
+		return new ModelAndView("newShop");
 	}
-	
+
 	@RequestMapping(value = "/createShop", method = RequestMethod.POST)
-	public String showCreateShop(Shop shop) {
-		shopService.createAdress(shop);
+	public String showCreateShop(@AuthenticationPrincipal User user, Shop shop) {
+		shopService.createShop(shop, user.getId());
 		return "profileUser";
 	}
-	
 }
